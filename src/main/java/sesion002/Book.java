690d45d0f92dadde8d2da5337cbe44c09b920616
Mahdie Montazeri <mmontazer1831@gmail.com>
2022-01-11 23:05:34 +0330

    add some propertied to address class and folan

--- src/main/java/sesion002/Book.java
+++ src/main/java/sesion002/Book.java
@@ -1,8 +1,22 @@
 package sesion002;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Created by Mahdie Montazeralzohoor on 1/11/2022 7:57 PM
  */
+@Data
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+
 
 }
--- src/main/java/sesion002/Role.java
+++ src/main/java/sesion002/Role.java
@@ -1,11 +1,17 @@
 package sesion002;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 /**
  * Created by Mahdie Montazeralzohoor on 1/11/2022 7:57 PM
  */
 @Entity
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
 }
